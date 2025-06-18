bfe464ae461489b1ba9cb185dc7580dd309e2e96
Hung <truongquochung0704@gmail.com>
2025-06-18 16:26:35 +0700

    [FEATURE] lấy danh sách phim theo tình trạng phim

--- MovieTheater/src/main/java/com/example/demo/controller/MovieController.java
+++ MovieTheater/src/main/java/com/example/demo/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.request.MovieRequest;
 import com.example.demo.entity.response.MovieResponse;
+import com.example.demo.enums.MovieStatus;
 import com.example.demo.service.MovieService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/movie")
@@ -25,4 +28,12 @@ public class MovieController {
         MovieResponse response = movieService.addMovie(movieRequest);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<MovieResponse>> getMoviesByStatus(@RequestParam("status")MovieStatus status) {
+        List<MovieResponse> movies = movieService.getMoviesByStatus(status);
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+    }
+
+
 }
