7d38d4c24d3094eb54e7225044d1ce48d978e319
Hung <truongquochung0704@gmail.com>
2025-07-16 16:33:23 +0700

    [FEAT] create cinema room

---
+++ MovieTheater/src/main/java/com/example/demo/entity/request/CinemaRoomRequest.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity.request;
+
+import com.example.demo.enums.FilmVersion;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.File;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CinemaRoomRequest {
+    @NotBlank(message = "Cinema room must not be blank")
+    private String cinemaRoomName;
+
+    @Min(value = 10000, message = "Base ticket price must be at least 10,000 VND")
+    private Long baseTicketPrice;
+
+    private String walkingLanePosition;
+    private Set<FilmVersion> supportedFilmVersions;
+}
