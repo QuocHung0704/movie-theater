cf99e850bdbc437de504a71c6127ecbdb366a69e
Hung <truongquochung0704@gmail.com>
2025-07-19 23:10:10 +0700

    [FEAT] get all cinema room

--- MovieTheater/src/main/java/com/example/demo/repository/CinemaRoomRepository.java
+++ MovieTheater/src/main/java/com/example/demo/repository/CinemaRoomRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.entity.CinemaRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CinemaRoomRepository extends JpaRepository<CinemaRoom, Long> {
     boolean existsByCinemaRoomNameAndIsDeletedFalse(String cinemaRoomName);
+    List<CinemaRoom> findByIsDeletedFalse();
 }
--- MovieTheater/src/main/java/com/example/demo/service/CinemaRoomService.java
+++ MovieTheater/src/main/java/com/example/demo/service/CinemaRoomService.java
@@ -1,8 +1,11 @@
-package com.example.demo.mapper;
+package com.example.demo.service;
 
 import com.example.demo.entity.CinemaRoom;
 import com.example.demo.entity.request.CinemaRoomRequest;
 
+import java.util.List;
+
 public interface CinemaRoomService {
     CinemaRoom createCinemaRoom(CinemaRoomRequest cinemaRoomRequest);
+    List<CinemaRoom> getAllCinemaRooms();
 }
